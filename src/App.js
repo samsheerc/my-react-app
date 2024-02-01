be7fcca3199b1cceacf920ea020f0c5f20cf69d4
samsheersamshi <samsheersamshi85@gmail.com>
2024-02-01 11:50:17 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import NavBar from './components/Navbar/NavBar';
 import './App.css';
+import Banner from './components/Navbar/Banner/Banner';
+import RowPost from './RowPost/RowPost';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <NavBar/>
+       <Banner/>
+       <RowPost/>
     </div>
   );
 }
---
+++ src/components/Navbar/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+       <div className='content'>
+         <h1 className='title'>MOVIE NAME</h1>
+         <div className='banner_buttons'>
+            <button className='button'>Play</button>
+            <button className='button'>my List</button>
+         </div>
+          <h1 className='description'>in publication in can wait it itxudheufygfgfgf47ryv</h1>
+       </div>
+     <div className='fade_bottom'></div>
+    </div>
+  )
+}
+
+export default Banner
